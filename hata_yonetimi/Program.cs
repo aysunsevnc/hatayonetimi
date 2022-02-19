9d9572c4ed09b044e7aaa68950b8a3bdd54aa59f
aysunsevnc <97788573+aysunsevnc@users.noreply.github.com>
2022-02-19 17:11:26 +0300

    Add files via upload

---
+++ hata_yonetimi/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace hata_yonetimi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Bir sayıyı giriniz:");
+                int sayi =Convert.ToInt32(Console.ReadLine());
+
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Hata:"+ ex.Message.ToString());
+                
+            }
+          //finally
+          //{ 
+          //  Console.WriteLine("İşlem Tamamlandı");
+          //}
+            try
+            {
+               // int a =int.Parse(null);
+              // int a =int.Parse("test");
+              int a =int.Parse("-200000000");
+
+            }
+            catch(ArgumentException ex)
+            {
+                Console.WriteLine("boş değer girdiniz");
+                Console.WriteLine(ex);
+
+            }
+            catch(FormatException ex)
+            {
+                Console.WriteLine("Veri tipi uygun değil");
+                Console.WriteLine(ex);
+            }
+            catch(OverflowException ex)
+            {
+                Console.WriteLine("Çok küçük yada çok büyük bir değer girdiniz");
+                Console.WriteLine(ex);
+            }
+            finally
+          { 
+           Console.WriteLine("İşlem Tamamlandı");
+          }
+        }
+    }
+}
